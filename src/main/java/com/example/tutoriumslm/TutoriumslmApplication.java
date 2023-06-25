274b0652f88223166dc9b683b947aada3faf3dd2
unknown <wi22b034@technikum-wien.at>
2023-06-25 10:18:38 +0200

    Initial commit

---
+++ src/main/java/com/example/tutoriumslm/TutoriumslmApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutoriumslm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoriumslmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoriumslmApplication.class, args);
+	}
+
+}
